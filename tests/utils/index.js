2e500f77df5c22890228ebb8b5ac57ec2379506e
Kamil Pula <kamil.pula.dev@gmail.com>
2024-08-15 19:37:33 +0200

    test: add test cases for vue-no-excessive-whitespaces rule
    tests: setup mocha and basic tests

---
+++ tests/utils/index.js
@@ -0,0 +1,25 @@
+import { RuleTester } from 'eslint'
+import vueParser from 'vue-eslint-parser'
+
+export const parserOptions = {
+  ecmaVersion: 2019,
+  sourceType: 'module',
+  ecmaFeatures: {
+    jsx: true,
+  },
+}
+
+export const ruleTester = new RuleTester({ languageOptions: { parserOptions } })
+
+export function createErrors(messageId, count = 1) {
+  const errors = []
+
+  for (let i = 0; i < count; i++)
+    errors.push({ messageId })
+
+  return errors
+}
+
+export function createVueTestCase({ code, output = '', errors = [] }) {
+  return { code, output, errors, languageOptions: { parser: vueParser }, filename: 'test.vue' }
+}
