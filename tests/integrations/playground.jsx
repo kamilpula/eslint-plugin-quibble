a454adcd71b05f26fc8d670346134bd8dda6b172
Kamil Pula <kamil.pula.dev@gmail.com>
2024-08-29 20:56:42 +0200

    test: add jsx and tsx playground

---
+++ tests/integrations/playground.jsx
@@ -0,0 +1,12 @@
+export default function HiMyNameIs({
+  name = 'Hi, my name is -',
+}) {
+  return (
+
+    <h1 className="thats an awfully hot coffee pot">
+      {name}
+      Slim Shady
+    </h1>
+
+  )
+}
